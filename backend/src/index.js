62af32a0115c37c484eae725bc4399b77b7a145d
SatyabratDeveloper <satyabrat.developer@gmail.com>
2025-01-23 20:09:16 +0530

    express configured and cors, cookie-parser and some more middlewares added

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,3 +1,9 @@
 import dotenv from "dotenv";
+import app from "./app";
 
 dotenv.config();
+
+const PORT = process.env.PORT;
+
+app.on("error", (error) => console.log("Express error:", error));
+app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
