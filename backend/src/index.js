755b78f4d61632d18b112c557087ccce9d255a68
SatyabratDeveloper <satyabrat.developer@gmail.com>
2025-01-24 10:33:40 +0530

    MongoDB connected

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,14 @@
 import dotenv from "dotenv";
-import app from "./app";
+import app from "./app.js";
+import connectDB from "./db/index.js";
 
 dotenv.config();
 
 const PORT = process.env.PORT;
 
-app.on("error", (error) => console.log("Express error:", error));
-app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
+connectDB()
+  .then(() => {
+    app.on("error", (error) => console.log("Express error:", error));
+    app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
+  })
+  .catch((error) => console.log("MongoDB connection failed:", error));
